45ec9042c3973dc8833712f3666fa93a6f2af9bf
gekishou <gonfuentes112@gmail.com>
2025-03-11 22:28:27 -0300

    add database

--- db/pool.js
+++ db/pool.js
@@ -1,1 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
+const { Pool } = require("pg");
+module.exports = new Pool({
+  host: process.env.host,
+  user: process.env.user,
+  database: process.env.user,
+  password: process.env.password,
+  port: process.env.password,
+});
